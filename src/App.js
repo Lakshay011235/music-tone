7dbd61557d1a4f650f90d196e1073a06bbaee237
Lakshay Sharma <107244796+Lakshay011235@users.noreply.github.com>
2023-08-22 18:35:25 +0530

    G5 Joyboy Returns

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import Guitar from './components/Guitar';
+// import Guitar from './components/Guitar';
 // import Piano from './components/Piano';
+import Joyboy from './components/Joyboy';
 
 function App() {
   
   return (
     <div className="App">
       {/* <Piano /> */}
-      <Guitar />
+      {/* <Guitar /> */}
+      <Joyboy />
     </div>
   );
 }
--- src/components/Joyboy.jsx
+++ src/components/Joyboy.jsx
@@ -1,9 +1,6 @@
-import React from 'react';
-import * as Tone from 'tone';
-import '../styles/guitartable.css';
+import React, { useState } from 'react'
 
-
-const GuitarTable = ({synth}) => {
+function Joyboy() {
   const strings = [
     'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4', 'C5', 'C#5', 'D5', 'D#5', 'E5',
     'B3', 'C4', 'C#4', 'D4', 'D#4', 'E4', 'F4', 'F#4', 'G4', 'G#4', 'A4', 'A#4', 'B4',
@@ -12,24 +9,6 @@ const GuitarTable = ({synth}) => {
     'A2', 'A#2', 'B2', 'C3', 'C#3', 'D3', 'D#3', 'E3', 'F3', 'F#3', 'G3', 'G#3', 'A3',
     'E2', 'F2', 'F#2', 'G2', 'G#2', 'A2', 'A#2', 'B2', 'C3', 'C#3', 'D3', 'D#3', 'E3',
   ];
-
-  const handleClick = (note) => {
-    if (synth !== null){
-      synth.triggerAttackRelease(note, '8n');
-    }
-    else {
-      alert("Synth is not available");
-    }
-  };
-
-
-  /**
-   * Returns the Note corresponding to the string and fret number
-  *  @param {Number} stringIndex - E4 => 0, B3=>1, G3=>2, D3=>3, A2=>4, E2=>5
-  *  @param {Number} fretIndex - default => 0 (open note), 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12 
-  *
-  *  @returns {String|Error} Note - eg."E4" or "D#3" | Error = Invalid note Index value
-  */
   const calcNote = (stringIndex, fretIndex) => {
     try {
       if (stringIndex >= 0 && stringIndex <=5) {
@@ -49,22 +28,26 @@ const GuitarTable = ({synth}) => {
     }
   };
 
-  calcNote()
-  return (
-    <div className="guitar-table">
-      {strings.map((note, index) => (
-        <div
-          className="string"
-          key={index}
-          onClick={() => handleClick(note)}
-        >
-          <div className="cell">
-            <span className="note disable-select">{note}</span>
-          </div>
-        </div>
-      ))}
-    </div>
-  );
-};
 
-export default GuitarTable;
+  const [stringIndex, setStringIndex] = useState(0);
+  const [fretIndex, setFretIndex] = useState(0);
+  const [noteIndex, setNoteIndex] = useState(0);
+  const showNote = () => {
+    setNoteIndex(calcNote(stringIndex, fretIndex));
+  };
+  return (
+    <div>
+      <form>
+        <input type='number' name='String' value={stringIndex} onChange={(e) => {setStringIndex(e.target.value)}}></input>
+        <input type='number' name='Fret' value={fretIndex} onChange={(e) => {setFretIndex(e.target.value)}}></input>
+      </form>
+      <div>
+        <button onClick={showNote}>Press me</button>
+      </div>
+      
+      <div>{noteIndex}</div>
+    </div>
+  )
+}
+
+export default Joyboy
